405dcd27b4ffa4d81de311ae8efedc0eefbe68f6
Me <1@1.ru>
2023-08-29 00:01:00 +0300

    тернарные операторы не очень красивые, переделал через переменную

--- hw_4/task_24.py
+++ hw_4/task_24.py
@@ -13,6 +13,7 @@ max_berry = 0
 kusts = [13, 5, 8, 1, 3, 15, 7, 11, 4, 16, 9]
 
 for i in range(len(kusts), 0, -1):
-    max_berry = (kusts[0 - i] + kusts[1 - i] + kusts[2 - i]) if max_berry < (kusts[0 - i] + kusts[1 - i] + kusts[2 - i]) else max_berry
+    tmp_step = kusts[0 - i] + kusts[1 - i] + kusts[2 - i]
+    max_berry = tmp_step if max_berry < tmp_step else max_berry
 
 print(max_berry)
